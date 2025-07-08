c0d0df50ca1f6060648d16660f27690315275224
Igor Todorovski <itodorov@ca.ibm.com>
2025-07-07 23:11:41 -0400

    Add record_format function to bpamio.h

--- services/src/bpamio.c
+++ services/src/bpamio.c
@@ -1020,3 +1020,19 @@ int deq_dataset_member(const char* ds, const char* wmem, const DBG_Opts* opts)
   free(qname);
   return rc;
 }
+
+record_format_t record_format(const FM_BPAMHandle* bh, const DBG_Opts* opts)
+{
+  if ((bh->dcb->dcbexlst.dcbrecfm & dcbrecf) && (bh->dcb->dcbexlst.dcbrecfm & dcbrecbr)) {
+    return RECORD_FORMAT_FB;
+  } else if (bh->dcb->dcbexlst.dcbrecfm & dcbrecf) {
+    return RECORD_FORMAT_F;
+  } else if ((bh->dcb->dcbexlst.dcbrecfm & dcbrecv) && (bh->dcb->dcbexlst.dcbrecfm & dcbrecbr)) {
+    return RECORD_FORMAT_VB;
+  } else if (bh->dcb->dcbexlst.dcbrecfm & dcbrecv) {
+    return RECORD_FORMAT_V;
+  } else if (bh->dcb->dcbexlst.dcbrecfm & dcbrecu) {
+    return RECORD_FORMAT_U;
+  }
+  return RECORD_FORMAT_UNKNOWN;
+}
